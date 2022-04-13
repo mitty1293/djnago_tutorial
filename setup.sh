09f6b56ee08bec675bde93dfd6662b2cb5bd6ddc
k-furumichi <k-furumichi@sakura.ad.jp>
2022-04-14 07:44:15 +0900

    setup作成

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# https://docs.djangoproject.com/ja/4.0/intro/install/
+# https://qiita.com/tetutaro/items/53b746412285920180b6
+# 色々インストールするスクリプトを書く
+# poetryでバージョンを分けたpythonインストール
+# DBインストール
+# djangoインストール

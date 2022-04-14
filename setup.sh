ad347f34bfa0f92d50c3222bfdc984c2e915fe74
k-furumichi <k-furumichi@sakura.ad.jp>
2022-04-14 09:51:38 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
+# django自体のインストールは↓
 # https://docs.djangoproject.com/ja/4.0/intro/install/
+# poetryについては↓
 # https://qiita.com/tetutaro/items/53b746412285920180b6
+# poetry+djangoで信用できそうなのは↓？
+# https://rasulkireev.com/managing-django-with-poetry/
+
 # 色々インストールするスクリプトを書く
 # poetryでバージョンを分けたpythonインストール
 # DBインストール
 # djangoインストール
+
+dockerで適当にdebianとかの環境作って実験してみる

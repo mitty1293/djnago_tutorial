615d24d07c2e92a0bb56350e204458362aac64c9
mitty1293 <k.fmichi@gmail.com>
2022-04-15 23:16:27 +0900

    fix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # djnago_tutorial
 [djangoの公式チュートリアル](https://docs.djangoproject.com/ja/4.0/intro/)をやってみる
 >Django アプリケーションを作成するときの典型的なワークフローは、 モデルを作成し、 admin サイトを組み上げてできるだけ早期に立ち上げ、スタッフ (や顧客) がデータを投入できるようにしておいてから、データを公開するための方法を開発してゆくというものです。
+## Referenses
+* https://rasulkireev.com/managing-django-with-poetry/
--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-# django自体のインストールは①↓
-# https://docs.djangoproject.com/ja/4.0/intro/install/
-# poetryについては②↓
-# https://qiita.com/tetutaro/items/53b746412285920180b6
-# poetry+djangoで信用できそうなのは③↓？
-# https://rasulkireev.com/managing-django-with-poetry/
+# pythonが無い場合は先にpythonインストール
+# poetryインストール
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
-# 色々インストールするスクリプトを書く
-# poetryでバージョンを分けたpythonインストール
-# DBインストール
-# djangoインストール
+# 自動で `~/.profile` に `export PATH="$HOME/.poetry/bin:$PATH"` が追加される
+# （$PATHにpoetry実行ファイルのディレクトリが追加される）
+# sourceコマンドで再ログインせずに反映させる
+source ~/.profile
 
-dockerで適当にdebianとかの環境作って実験してみる
-③の通り進めればdjangoもいけそう。（①とも照合済）
+# 依存関係インストール（django等）
+poetry install
+
+# プロジェクトを作成する場合、以下で<PROJECTNAME>ディレクトリを作成する
+# poetry run django-admin startproject <PROJECTNAME>

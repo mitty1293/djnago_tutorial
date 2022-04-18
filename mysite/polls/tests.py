ba9eaeca07b8d9cad51798551fddcd069bb4f9bd
k-furumichi <k-furumichi@sakura.ad.jp>
2022-04-19 06:07:06 +0900

    その5　まで完了

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,99 @@
+import datetime
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_question(self):
+        """
+        質問が存在しない場合、適切なメッセージが表示される
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+    
+    def test_past_question(self):
+        """
+        pub_dateが過去である質問がインデックスページに表示される
+        """
+        question = create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_future_question(self):
+        """
+        pub_dateが未来である質問はインデックスページに表示されない
+        """
+        question = create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        過去の質問と未来の質問の両方が存在する場合、過去の質問のみが表示される
+        """
+        question = create_question(question_text="Past question", days=-30)
+        create_question(question_text="Future question", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+
+    def test_two_past_questions(self):
+        """
+        質問インデックスページには、複数の質問が表示される場合がある
+        """
+        question1 = create_question(question_text="Past question1", days=-30)
+        question2 = create_question(question_text="Past question2", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        pub_dateが未来である質問の詳細表示は404 not foundを返す
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        pub_dateが過去である質問の詳細表示は質問文を表示する
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() はpub_dateが未来日の場合はFalseを返す
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() はpub_dateが1日以上古い場合はFalseを返す
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() はpub_dateが1日以内の場合はTrueを返す
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
